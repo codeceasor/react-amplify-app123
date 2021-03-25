48dea0263e2091a71680620595b425bc51296f3c
codeceasor <simransharma271196@gmail.com>
2021-03-25 11:55:04 +0530

    updated headline

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {Formik, Field, Form} from 'formik';
+//import {Formik, Field, Form} from 'formik';
 
 function App() {
   return (
@@ -11,22 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           <h2>hello</h2>
         </p>
-        <h3> Signup form </h3>
-        <Formik 
-        initialValues={{ name: '', email:'',password: ''}}
-        onSubmit={(values) => 
-          {
-            console.log("values,", values)
-          }
-        }
-        >
-         <Form>
-           <Field name="name" placeholder="Enter name"/>
-           <Field name="email" type="email" placeholder="Enter email"/>
-           <Filed name="password" type="password" placeholder="Enter password"/>
-           <button type="submit"> Signup </button>
-           </Form> 
-        </Formik>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
