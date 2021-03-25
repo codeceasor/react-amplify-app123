1e3bef4db2fe14485a795cfef4de90e4bfc3fc7d
codeceasor <simransharma271196@gmail.com>
2021-03-25 11:58:11 +0530

    updated headline

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <h2>hello</h2>
+          <h2>hello simran in your first app</h2>
         </p>
         
         <a
